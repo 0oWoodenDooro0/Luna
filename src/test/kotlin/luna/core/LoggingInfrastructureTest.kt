47ded3c8233861d505505dde183358fefbd82ad2
Vincent <vincent031525@gmail.com>
2026-04-12 17:57:00 +0800

    feat(logging): create logback.xml with RollingFileAppender

--- src/test/kotlin/luna/core/LoggingInfrastructureTest.kt
+++ src/test/kotlin/luna/core/LoggingInfrastructureTest.kt
@@ -2,6 +2,7 @@ package luna.core
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class LoggingInfrastructureTest {
     @Test
@@ -11,8 +12,11 @@ class LoggingInfrastructureTest {
     }
 
     @Test
-    fun `should have logstash encoder available`() {
-        val encoder = Class.forName("net.logstash.logback.encoder.LogstashEncoder")
-        assertNotNull(encoder)
+    fun `should have logback configuration with RollingFileAppender`() {
+        val factory = org.slf4j.LoggerFactory.getILoggerFactory() as ch.qos.logback.classic.LoggerContext
+        val rootLogger = factory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+        val appender = rootLogger.getAppender("JSON_FILE")
+        assertNotNull(appender, "JSON_FILE appender should be configured")
+        assertTrue(appender is ch.qos.logback.core.rolling.RollingFileAppender<*>, "Appender should be RollingFileAppender")
     }
 }
